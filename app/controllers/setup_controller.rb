6077aa6131a679d1b289fbf012a1673772334a5f
Parker P <exxcaelum@gmail.com>
2016-08-03 18:07:52 -0600

    removing artifact

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -1,9 +1,6 @@
 class SetupController < ApplicationController
 
   def show
-    if current_user.categories.first == nil
-      #redirect_to
-    else
       @user = current_user
       @subscriptions = Subscription.get_subscriptions(current_user)
       @categories = current_user.categories
