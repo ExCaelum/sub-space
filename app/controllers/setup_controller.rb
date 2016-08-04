7de49867d2b9d0e65eed45902562cd33bcea055c
Parker P <exxcaelum@gmail.com>
2016-08-03 18:11:53 -0600

    syntax issue

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -1,10 +1,9 @@
 class SetupController < ApplicationController
 
   def show
-      @user = current_user
-      @subscriptions = Subscription.get_subscriptions(current_user)
-      @categories = current_user.categories
-    end
+    @user = current_user
+    @subscriptions = Subscription.get_subscriptions(current_user)
+    @categories = current_user.categories
   end
 
 end
